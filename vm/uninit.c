8f506a7b42e472a080c665cc3c1dcf0afc92ddf5
goodsmajungle <ma546@naver.com>
2023-10-19 07:48:44 +0000

    uninit_destroy 27/141

--- vm/uninit.c
+++ vm/uninit.c
@@ -10,6 +10,7 @@
 
 #include "vm/vm.h"
 #include "vm/uninit.h"
+#include "threads/malloc.h"
 
 static bool uninit_initialize (struct page *page, void *kva);
 static void uninit_destroy (struct page *page);
@@ -62,7 +63,11 @@ uninit_initialize (struct page *page, void *kva) {
  * PAGE will be freed by the caller. */
 static void
 uninit_destroy (struct page *page) {
-	struct uninit_page *uninit UNUSED = &page->uninit;
+	struct uninit_page *uninit = &page->uninit;
+	if (uninit->type == VM_UNINIT){
+		free(page);
+	}
+	return;
 	/* TODO: Fill this function.
 	 * TODO: If you don't have anything to do, just return. */
 }
